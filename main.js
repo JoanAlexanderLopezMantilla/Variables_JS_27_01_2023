9ed5b51ebfbdf4ad6d8292872c1791ac4137e8e6
JoanAlexanderLopezMantilla <joan_lopez_77@hotmail.com>
2023-02-02 21:03:43 -0500

    Variables

---
+++ main.js
@@ -0,0 +1,20 @@
+// String Cadenas de texto
+let ciudadNacimiento = "Bucaramanga"
+
+//Number Valores numéricos.
+let edadUsuario = 35
+
+//Boolean Representa una entidad lógica y puede tener dos valores: true y false.
+let estudianteComfenalco = true
+
+//Object Un valor en memoria al que podemos acceder por un identificador
+let nombreEstudiantes = {
+    estudiante1: "Joan",
+    estudiante2: "Miguel",
+    estudiante3: "Andrea",
+}
+
+console.log(`La ciudad de nacimiento es ${ciudadNacimiento}`)
+console.log(`La edad del usuario es:  ${edadUsuario}`)
+console.log(`El estudiante es de Comfenmalco:  ${estudianteComfenalco}`)
+console.log(`Los Nombres de los estudiantes son ${nombreEstudiantes.estudiante1}, ${nombreEstudiantes.estudiante2}, ${nombreEstudiantes.estudiante3}`)

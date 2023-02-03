5d27fcb10fa3bcf0ef9f706f9469c4a1fdbb503b
JoanAlexanderLopezMantilla <joan_lopez_77@hotmail.com>
2023-02-02 22:55:41 -0500

    Mostrar datos en las ventanas emergentes

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-// Se crea variable
-let nombre = "Joan Alexander Lopez Mantilla"
+//Se le asigna valor a la variable nombre
+let nombre = "Joan Alexander Lopez mantilla";
 
-// Se prueban todas los tipos de consola del navegador
+//Ventana para mostrar
+alert(nombre);
 
-console.log(nombre);//El mas utilizado para mostrar en consola del navegador
-console.warn(nombre);//Muestra una advertencia
-console.error(nombre);//Muestra un error
-console.info(nombre);//Lo mismo que console.log
+//Ventana tipo booleano
+confirm(nombre);
 
-console.group(" Este es el grupo de consolas");
-    console.log(nombre);//El mas utilizado para mostrar en consola del navegador
-    console.warn(nombre);//Muestra una advertencia
-    console.error(nombre);//Muestra un error
-    console.info(nombre);//Lo mismo que console.log
-console.groupEnd();
-
-console.table([nombre])
-console.table([{nombre_variable: nombre}]);
-
-//console.clear();
-
-console.time();
-    console.log("Este es el tiempo que demora en ejecutar");
-console.timeEnd();
+//Ventana para pedir datos
+prompt("Ingrese su nombre");
